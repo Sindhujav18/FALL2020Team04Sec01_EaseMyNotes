0e45e0cc9cad3be23014a5e6ca6efa5404659f88
sindhu rani goli <saisindhurani@gmail.com>
2020-09-29 18:54:04 -0500

    changed code in login activity

--- app/src/main/java/com/example/fall2020team04sec01_easemynotes/LoginActivity.java
+++ app/src/main/java/com/example/fall2020team04sec01_easemynotes/LoginActivity.java
@@ -3,12 +3,17 @@ package com.example.fall2020team04sec01_easemynotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText useranme;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
