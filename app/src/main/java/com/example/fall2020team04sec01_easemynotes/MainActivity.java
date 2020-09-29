0c67eb3f09398480e8a234402f81d81f6e639160
Sindhu <sindhujav18@gmail.com>
2020-09-29 02:16:34 -0500

    Added intents in the launch screen to navigate to login and register activity

--- app/src/main/java/com/example/fall2020team04sec01_easemynotes/MainActivity.java
+++ app/src/main/java/com/example/fall2020team04sec01_easemynotes/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.fall2020team04sec01_easemynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login =  findViewById(R.id.btn_login);
+        register = findViewById(R.id.btn_mainregister);
+
+        login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        }
+
+        );
+
+        register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        }
+
+        );
     }
 }
