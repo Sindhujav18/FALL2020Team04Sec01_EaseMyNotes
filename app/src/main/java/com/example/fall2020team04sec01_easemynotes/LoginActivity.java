5e505e8ab596d49bf42d34f43c82f551ff904752
Sindhu <sindhujav18@gmail.com>
2020-09-29 17:56:09 -0500

    Created intent to go to home activity

--- app/src/main/java/com/example/fall2020team04sec01_easemynotes/LoginActivity.java
+++ app/src/main/java/com/example/fall2020team04sec01_easemynotes/LoginActivity.java
@@ -2,13 +2,40 @@ package com.example.fall2020team04sec01_easemynotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button login;
+    TextView register;
+    TextView notRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username= findViewById(R.id.et_username);
+        password= findViewById(R.id.et_password);
+        login=  findViewById(R.id.btn_login);
+        register= findViewById(R.id.tv_register);
+        notRegister= findViewById(R.id.tv_notregister);
+
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent= new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        }
+
+        );
+
     }
 }
