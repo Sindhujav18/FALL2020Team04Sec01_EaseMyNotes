472612bc45e271ee4ab767dc19a4635b9d4d622d
Sindhu <sindhujav18@gmail.com>
2020-09-29 00:35:40 -0500

    Created second activity for login, added textview, added vector assets for login and password

---
+++ app/src/main/java/com/example/fall2020team04sec01_easemynotes/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.fall2020team04sec01_easemynotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
